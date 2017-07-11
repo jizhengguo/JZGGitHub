1b3e9f77dac66c6cd68cbdd433c5c2f49aeffcd5
hm26 <xxxx@qq.com>
2017-07-11 09:50:51 +0800

    Initial Commit

---
+++ 支付宝/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  支付宝
+//
+//  Created by apple on 2017/7/11.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
