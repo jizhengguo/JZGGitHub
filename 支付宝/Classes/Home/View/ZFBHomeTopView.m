c7b66c8347c20a1e66a45f4251fc6ece06a779bb
hm26 <xxxx@qq.com>
2017-07-11 15:34:28 +0800

    08-首页顶部视图添加按钮

--- 支付宝/Classes/Home/View/ZFBHomeTopView.m
+++ 支付宝/Classes/Home/View/ZFBHomeTopView.m
@@ -7,15 +7,44 @@
 //
 
 #import "ZFBHomeTopView.h"
+#import "NSAttributedString+Additon.h"
 
 @implementation ZFBHomeTopView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+// 添加四个大按钮""
+-(void)awakeFromNib {
+    [super awakeFromNib];
+    
+    [self setupUI];
 }
-*/
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setupUI];
+    }
+    return self;
+}
+
+- (void)setupUI {
+    
+    UIButton *scanBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    
+    // 创建属性文本"把图片和文本拼接到一起"富文本
+    NSAttributedString *attr = [NSAttributedString imageTextWithImage:[UIImage imageNamed:@"home_scan"] imageWH:35 title:@"扫一扫" fontSize:14 titleColor:[UIColor whiteColor] spacing:8];
+    // 设置按钮的属性文本
+    [scanBtn setAttributedTitle:attr forState:UIControlStateNormal];
+    scanBtn.frame = CGRectMake(0, 0, 115, 115);
+    
+    // 设置按钮中文字换行
+    scanBtn.titleLabel.numberOfLines = 0;
+    // 设置按钮中文字居中
+    scanBtn.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [self addSubview:scanBtn];
+    
+}
+
+
 
 @end
